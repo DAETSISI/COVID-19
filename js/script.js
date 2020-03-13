ec7cc023779a8b92e8f02344137b33a50c56debd
Yuuba <32301165+yuba17@users.noreply.github.com>
2020-03-13 13:32:52 +0100

    PaginaResponsiveV2
    Falta por hacer el footer, y los colores de las cajas. Está arreglado que las preguntas se metan por encima.

---
+++ js/script.js
@@ -0,0 +1,11 @@
+jQuery('document'.ready(function($){
+
+var menuBTn = $('.menu-icon'),
+    menu = $('.navigation ul');
+             
+menuBtn.click(function(){
+    menu.addClass('show');
+});
+
+                        });
+    
