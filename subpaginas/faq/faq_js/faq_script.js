effb524977b3ce937f234a6ad19ac454b09e5c44
Yuuba <yubargp@gmail.com>
2020-03-16 01:21:49 +0100

    Orden de los Archivos
    Se ha ordenado los archivos obtenidos por el merged, hallar solución para no hacer esto tras cada merged.

---
+++ subpaginas/faq/faq_js/faq_script.js
@@ -0,0 +1,26 @@
+* { margin: 0; padding: 0; box-sizing: border-box;}
+body { background: url(../img/fondo.jpg) no-repeat center center fixed; background-size: cover; font-family: arial, helvetica, sans-serif; }
+#contenedor { width: 80%; margin: 0 auto;}
+#header { position: relative; padding-top: 20px; }
+.sticky {  position: fixed; top: 0;  width: 100% }
+.sticky + .content {  padding-top: 102px;}
+#logo img { padding: 1% 1%; width: 10%;}
+#menu { width: 100%; text-align: center;}
+#menu ul { list-style-type: none; }
+#menu ul li { display: inline-block; }
+#menu ul li a { color: white; text-decoration: none; padding: 5px 15px; font-size: 24px; }
+#menu ul li a:hover, #menu ul li a#activo:hover { border-bottom: 2px solid #FFF }
+#menu ul li a#activo { border-bottom: 1px solid #FFF}
+#mensaje { clear: both; height: 360px; position: relative; text-align:center; color: #FFF; }
+#mensaje { position: relative; font-size: 32px; text-transform: uppercase; font-weight: normal; top: 100px;}
+#mensaje p { position: relative; font-size: 18px; letter-spacing: 4px; top: 10px; }
+#principal, #footer { padding: 10px 0; }
+#principal img { width: 50%; margin-left: 20px; align-content: center }
+#principal h1 {color: #F60; background: url(../img/nube-naranja.png) no-repeat left center; padding-left: 30px; margin-bottom: 15px; }
+#principal p { font-size: 12px; color: #555; letter-spacing: 1px; line-height: 2.2; }
+#principal hr {border: none; border-top: 2px solid #CCC; margin-top: 50px; margin-bottom: 16px; }
+#principal iframe {width: 100%; height: 250px; }
+#footer {background: #CCC; text-align: center; position: relative; top: 2000px}
+#footer p { color: #555; font-size: 12px;}
+#footer img {position: absolute; right: 5px; top 50%; margin-top: -8px}
+#redes { position: absolute; right: 0; top: 410px; width: 36px}
