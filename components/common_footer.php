ee56ecae8db0000da7319390417d884722d273b5
Yuuba <yubargp@gmail.com>
2020-03-19 09:44:39 +0100

    Separados Footer y nav
    Se han sacado a componentes comunes ambos  elementos.

---
+++ components/common_footer.php
@@ -0,0 +1,41 @@
+<?php
+?>
+
+<style type="text/css">
+
+    }
+
+</style>
+
+<div class="footer">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-6">
+                <div id="Contacto">
+                    <h5>Contacto</h5>
+                    <p><strong>Delegación de Alumnos ETSISI</strong><br/>
+                        <strong>Universidad Politécnica de Madrid</strong><br/>
+                        <strong>Teléfono:</strong> 91 067 35 19<br/>
+                        <strong>Email:</strong> dalum.etsisi@gmail.com<br/>
+                        <strong>Web:</strong> daetsisi.es</p>
+                </div>
+            </div>
+            <div class="col-md-6">
+                <div id="RRSS">
+                    <h5>¡Conoce Nuestras Redes Sociales!</h5>
+                    <a href="https://twitter.com/dalumETSISI?s=20" alt="Twitter" target="_blank">
+                        <i class="fab fa-twitter"></i> Twitter
+                    </a><br>
+                    <a href="https://www.instagram.com/dalumetsisi/" alt="Instagram" target="_blank">
+                        <i class="fab fa-instagram"></i> Instagram
+                    </a><br>
+                    <a href="https://t.me/canaldaetsisi" alt="Canal Telegram" target="_blank">
+                        <i class="fab fa-telegram"></i> Canal de Telegram
+                    </a>
+                </div>
+            </div>
+        </div>
+        <hr>
+        <p id="copyRight">&copy;2020 Delegación de Alumnos ETSISI - Todos los derechos reservados</p>
+    </div>
+</div>
--- etsisi.php
+++ etsisi.php
@@ -22,45 +22,13 @@
 
         <h1>Medidas de la ETSI Sistemas Informáticos</h1>
         <hr>
-        <p>Queridos compañeros, desde la Delegación de Alumnos hemos habilitado esta plataforma con el objetivo de comunicar de una manera más eficaz y sencilla el plan de acción que se está realizando desde la Escuela.
+        <p>Queridos compañeros,<br> Desde la Delegación de Alumnos hemos habilitado esta plataforma con el objetivo de comunicar de una manera más eficaz y sencilla el plan de acción que se está realizando desde la Escuela.
             También, podréis trasladarnos vuestras preocupaciones sobre la metodología, material o forma en la que se están gestionando las diferentes asignaturas.</p>
 
 
     </div>
 </main>
-<footer class="footer">
-    <div class="container">
-        <div class="row">
-            <div class="col-md-6">
-                <div id="Contacto">
-                    <h5>Contacto</h5>
-                    <p><strong>Delegación de Alumnos ETSISI</strong><br/>
-                        <strong>Universidad Politécnica de Madrid</strong><br/>
-                        <strong>Teléfono:</strong> 91 067 35 19<br/>
-                        <strong>Email:</strong> dalum.etsisi@gmail.com<br/>
-                        <strong>Web:</strong> daetsisi.es</p>
-                </div>
-            </div>
-            <div class="col-md-6">
-                <div id="RRSS">
-                    <h5>¡Conoce Nuestras Redes Sociales!</h5>
-                    <a href="https://twitter.com/dalumETSISI?s=20" alt="Twitter" target="_blank">
-                        <i class="fab fa-twitter"></i> Twitter
-                    </a><br>
-                    <a href="https://www.instagram.com/dalumetsisi/" alt="Instagram" target="_blank">
-                        <i class="fab fa-instagram"></i> Instagram
-                    </a><br>
-                    <a href="https://t.me/canaldaetsisi" alt="Canal Telegram" target="_blank">
-                        <i class="fab fa-telegram"></i> Canal de Telegram
-                    </a>
-                </div>
-            </div>
-        </div>
-        <hr>
-        <p id="copyRight">&copy;2020 Delegación de Alumnos ETSISI - Todos los derechos reservados</p>
-    </div>
-</footer>
-
+<?php include 'components/common_footer.php'; ?>
 <div id="scripts">
     <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
