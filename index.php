1c96940c523e9b065587120e3b574193ba108d7b
Alberto Guerrero Martín <alberto170693@gmail.com>
2020-03-22 23:44:53 +0100

    New index texts

--- index.php
+++ index.php
@@ -18,25 +18,36 @@
                     y toda la información académica disponible que tenemos sobre la situación que estamos viviendo en relación al coronavirus (COVID-19).
                     <br>
                     <br>
+                    En esta página podréis consultar todos los cambios que las distintas asignaturas de los grados de la escuela van aplicando para adaptarse al contexto de docencia telemática provocado por la crisis del COVID-19.
+                    No obstante, queremos recalcar que <b>toda la información que encontraréis aquí está simplemente a título informativo, y es susceptible de ser modificada</b>, por lo que os aconsejamos permanecer atentos en todo momento
+                    a las indicaciones de vuestros/as profesores/as, así como a los medios de comunicación oficiales de la UPM.
+                    <br>
+                    <br>
+                    <b>Si encontráis cualquier fallo</b> en la información que está aquí publicada, o alguna discordancia con la información que os hayan dado los/as profesores/as de alguna asignatura, os rogamos que por favor <b>nos los comuniquéis
+                        lo antes posible a <a href="mailto:dalum.etsisi@gmail.com">dalum.etsisi@gmail.com</a></b>, para que podamos subsanarlo. Os agradeceremos enormemente cualquier colaboración.
+                    <br>
+                    <br>
                     Así mismo, a través de esta página podréis trasladarnos <b>vuestra valoración</b> sobre como están adaptando las diferentes asignaturas la metodología docente, el material, las pruebas de evaluación, etc.
                     <br>
                     <br>
-                    No obstante, recordad que para cualquier problema o queja importante, debéis enviarnos un <a href="https://evalua.daupm.es/evalua" target="_blank"><u><b>parte EVALUA</b></u></a>.
+                    Y, por último, recordad que para cualquier problema o queja importante, debéis enviarnos un <a href="https://evalua.daupm.es/evalua" target="_blank"><u><b>parte EVALUA</b></u></a>.
                 </div>
                 <hr style="border-top: 1px solid black !important; display:block"/>
 
                 <div style="text-align: justify">Esta plataforma consta de varias secciones:
                     <ul>
-                        <li><b>Sección ETSISI:</b> toda la información académica de la escuela sobre los <b>cambios en las asignaturas</b> para adaptarse a esta situación excepcional.</li>
-                        <li><b>Sección FAQs:</b> un recopilatorio de las preguntas más frecuentes que nos habéis realizado.</li>
-                        <li><b>Sección Frena la curva:</b> recomendaciones, consejos y tips para frenar la expansión del virus.</li>
-                        <li><b>Sección Documentación:</b> recopilatorio de todos los comunicados, Reales Decretos, Resoluciones Rectorales y documentos en general relativos a la crisis del coronavirus, del ámbito de la representación estudiantil, Universidad, Comunidad Autónoma y Estado.</li>
+                        <li><b><a href="/etsisi">ETSISI</a>:</b> toda la información académica de la escuela sobre los <b>cambios en las asignaturas</b> para adaptarse a esta situación excepcional.</li>
+                        <li><b><a href="/faq">FAQs</a>:</b> un recopilatorio de las preguntas más frecuentes que nos habéis realizado.</li>
+                        <li><b><a href="/flc">Frena la curva</a>:</b> recomendaciones, consejos y tips para frenar la expansión del virus.</li>
+                        <li><b><a href="/documentacion">Documentación</a>:</b> recopilatorio de todos los comunicados, Reales Decretos, Resoluciones Rectorales y documentos en general relativos a la crisis del coronavirus, del ámbito de la representación estudiantil, Universidad, Comunidad
+                            Autónoma
+                            y Estado.</li>
                     </ul>
                 </div>
 
                 <hr style="border-top: 1px solid black !important; display:block">
 
-                <div>Recuerda que puedes encontrar más información oficial a través de los siguientes enlaces:<br>
+                <div style="text-align: justify">Recuerda que puedes encontrar más información oficial a través de los siguientes enlaces:<br>
                     <ul id="lista">
                         <li>
                             <a href="https://www.mscbs.gob.es/profesionales/saludPublica/ccayes/alertasActual/nCov-China/home.htm" target="_blank">Ministerio de Sanidad</a>
