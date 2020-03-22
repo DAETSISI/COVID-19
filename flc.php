f01249b990b65874815cadf704117cc756bec7ad
Alberto Guerrero Martín <alberto170693@gmail.com>
2020-03-23 00:17:56 +0100

    Fixes on FLC section

--- flc.php
+++ flc.php
@@ -9,11 +9,13 @@
         outline: 0;
         box-shadow: 0 0 0 0rem rgba(0, 0, 0, 0);
     }
+
     .btn-pdf {
         background-color: #17a2b8;
         color: white !important;
         margin-bottom: 0.8rem;
     }
+
     a {
         color: #000000;
         text-decoration: none;
@@ -31,7 +33,7 @@
         <div class="row">
 
 
-            <div class="col-md-6">
+            <div class="col-md-6" style="text-align: justify">
                 <p>
 
                     <a href="https://www.mscbs.gob.es/campannas/campanas20/coronavirus.htm" target="_blank"><b>#EsteVirusLoParamosUnidos</b></a><br>
@@ -48,21 +50,26 @@
                         Lo paramos si ayudas y haces caso a nuestros profesionales<br>
 
                         Lo paramos cuando confías en que vamos a salir de esto <br>
-                   </i>
+                    </i>
 
                     <br>
                     Desde la <b>Delegación de Alumnos de la ETSI Sistemas Informáticos</b> animamos a todos nuestros compañeros, profesores y al
                     resto de la Comunidad Universitaria, a seguir las indicaciones del Gobierno y de la Comunidad de Madrid.
                     <br> <br>
-                    Asimismo, entendemos que es una situación complicada, sin embargo instamos a todos los profesores a que den docencia telemática,
-                    o graben vídeos para que puedan ser reproducidos posteriormente, y a que se suba material didáctico al moodle, aparte del habitual del formato presencial.
+                    Entendemos que es una situación complicada, no obstante instamos a todo el profesorado a que impartan <b>docencia telemática</b>,
+                    o graben <b>vídeos</b> para que puedan ser reproducidos posteriormente, así como a utilizar la plataforma Moodle para subir <b>material didáctico específicamente diseñado</b> para esta
+                    situación de docencia telemática, adicionalmente al ya existente para el formato presencial.
                     <br> <br>
-                    Pedimos, que los cambios que se produzcan en los criterios de calificación estén siempre orientados a la salvaguarda de la docencia, y miren por el
-                    bienestar de los estudiantes. Entendemos que la cancelación de los exámenes y forzar al desarrollo de exámenes finales no es el camino.
+                    Además, pedimos que los cambios que se produzcan en los criterios de evaluación se hagan entendiendo que
+                    <b>esta situación no es fácil para nadie</b>.
+                    Creemos que opciones como la cancelación de las pruebas de evaluación continua, dejando a los y las estudiantes
+                    como única opción los exámenes finales, no son en absoluto las soluciones adecuadas que se requieren en una situación como la actual.
                     <br> <br>
-                    Sabemos que es un cambio de docencia enorme, y nos gustaría agradecer a todos aquellos que ya lo han hecho.
+                    Así mismo, somos conscientes de la dificultad que entraña también para el profesorado el tener que realizar todos estos cambios, y nos gustaría
+                    <b>agradecer enormemente a todos aquellos que ya los han hecho</b>, así como ponernos a disposición de todo el colectivo del PDI para colaborar
+                    con nuestra opinión en el desarrollo de estos cambios.
                     <br> <br>
-                    Por otra parte, con el objetivo de amenizar, y encontrar una ocupación en este periodo, recomendamos algunos enlaces y
+                    Por otra parte, con el objetivo de amenizar un poco la cuarentena, y encontrar una ocupación para los ratos libres de este periodo, os recomendamos algunos enlaces y
                     páginas web a través de las cuáles podemos seguir formándonos.
                 </p>
                 <ul>
@@ -72,6 +79,7 @@
                             <li><a href="https://es.coursera.org/" target="_blank">Coursera</a></li>
                             <li><a href="https://www.edx.org/es" target="_blank">Edx</a></li>
                             <li><a href="https://unity.com/products/learn-premium" target="_blank">Desarrollo de videojuegos con unity</a></li>
+                            <li><a href="https://learndigital.withgoogle.com/activate/courses" target="_blank">Google Actívate</a></li>
                         </ul>
                     </li>
 
@@ -120,7 +128,7 @@
                                         <a href="./documentacion/Orientaciones_colegiopsicologia_cuarentena.pdf" target="_blank" alt="Recomendaciones Psicóloga">Orientaciones para la gestión psicológica de la cuarentena por el coronavirus</a></b>
                             </div>
                             <div class="col-sm-8">
-                                <p class="card-text fecha" style="margin-bottom: -0.2rem;">13/03/2020</p>
+                                <p class="card-text fecha" style="margin-bottom: -0.2rem;">15/03/2020</p>
                                 <p class="card-text órgano" style="margin-bottom: -0.2rem;">Colegio Oficial de la Psicología de Madrid</p>   <!--Quedaría mejor poner Gabinete Psicológico ETSIAAB -->
                             </div>
                             <div class="col-sm-4" style="text-align: end;">
@@ -137,7 +145,6 @@
                 <img src="./documentacion/cartelCoronavirus.jpg" width="100%" height="100%"/>
             </div>
         </div>
-
 
 
     </div>
